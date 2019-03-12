b9132e3f5d4cc8351a9948e009f76672706fb50b
Fabio Chiarini <chiarrss@gmail.com>
2019-03-12 23:01:04 +0100

    First implementation of the showPage function
    Implemented the showPage function to display the first 10 students of the list. Function is pretty messy so it'll need a lot of refractoring. For now the focus was just to get it to work.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// populate the students list with their respective properties
 let students = [{}];
 
+// construct the student list with their respective details
 const studentsList = document.querySelectorAll('LI');
 for (var i = 0; i < studentsList.length; i += 1){
   const studentDetails = document.getElementsByClassName('student-details')[i];
@@ -27,22 +27,55 @@ for (var i = 0; i < studentsList.length; i += 1){
 
 }
 
-console.log(students);
+console.log(students)
+
+// function to show just the students for that page (10 students for page)
+function showPage () {
+  const div = document.querySelector('div');
+  const remove = document.querySelector('ul');
+  div.removeChild(remove);
+  const ul = document.createElement('ul');
+  ul.className = 'student-list';
+  div.appendChild(ul);
+  for (var i = 0; i < 10; i+= 1){
+    const li = document.createElement('li');
+    li.className = 'student-item cf';
+    ul.appendChild(li);
+
+    const divStudent = document.createElement('div');
+    divStudent.className = 'student-details';
+    li.appendChild(divStudent);
+
+    const displayImg = document.createElement('img');
+    displayImg.className = 'avatar';
+    displayImg.src = students[i].img;
+    divStudent.appendChild(displayImg);
+
+    const displayName = document.createElement('h3');
+    displayName.innerHTML = students[i].name;
+    divStudent.appendChild(displayName);
+
+    const displayEmail = document.createElement('span');
+    displayEmail.className = 'email';
+    displayEmail.innerHTML = students[i].email;
+    divStudent.appendChild(displayEmail);
+
+    const divStudentJoined = document.createElement('div');
+    divStudentJoined.className = 'joined-details';
+    li.appendChild(divStudentJoined);
+
+    const displayJoinDate = document.createElement('span');
+    displayJoinDate.className = 'date';
+    displayJoinDate.innerHTML = students[i].joined;
+    divStudentJoined.appendChild(displayJoinDate);
+  }
+
+showPage();
+
+}
 
 
-/*
-UL
-<li class="student-item cf">
 
-
-    <div class="joined-details">
-           <span class="date">Joined 07/15/15</span>
-   </div>
-
-</li>
-UL
-
-*/
 
 /***
    Create the `showPage` function to hide all of the items in the
